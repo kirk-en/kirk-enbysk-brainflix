c2f7fe18a14ff4a4f3142c5ea9970fd895453fa0
Kirk Enbysk <kirkenbysk@gmail.com>
2024-05-31 15:40:15 -0400

    modify form to leave comments

--- src/components/CommentsField/CommentsField.jsx
+++ src/components/CommentsField/CommentsField.jsx
@@ -1,4 +1,5 @@
 import "./CommentsField.scss";
+import profileImage from '../../assets/images/Mohan-muruge.jpg'
 
 const CommentsField = () => {
   return (
@@ -7,7 +8,7 @@ const CommentsField = () => {
         <div className="comments__flex">
           <img
             className="comments__profile-image"
-            src="./assets/images/Mohan-muruge.jpg"
+            src={profileImage}
             alt="profile picture"
           />
           <form className="comments__form">
@@ -18,26 +19,17 @@ const CommentsField = () => {
                 type="text"
                 name="name"
                 placeholder="Enter your name"
-                autocomplete="off"
+                autoComplete="off"
                 required
               />
-            </label>
-            <label className="comments__label">
-              <span className="comments__text">comment</span>
-              <textarea
-                className="comments__field comments__field--no-resize"
-                name="comment"
-                placeholder="Add a new comment"
-                required
-              ></textarea>
             </label>
             <button type="submit" className="comments__btn">
               comment
             </button>
           </form>
         </div>
-        <div className="divide-line divide-line--pad"></div>
       </section>
+      <div className="divide-line divide-line--pad"></div>
     </>
   );
 };
