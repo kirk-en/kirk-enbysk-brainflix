85611d2909ecbe8750ae6fe147ee7bb4989caf7e
Kirk Enbysk <kirkenbysk@gmail.com>
2024-05-30 23:28:16 -0400

    connect and display props for video list items

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -6,14 +6,17 @@ const VideoList = ({ videosData, setVideo }) => {
 
   return (
     <>
-      <ul>
-        {videosData.map((video) => {
-          return <VideoListItem
-          video = {video}
-          setVideo = {setVideo}
-          />
-        })}
-      </ul>
+    <aside className="video-list">
+      <h2 className="video-list__title">next videos</h2>
+        <ul className="video-list__list">
+          {videosData.map((video) => {
+            return <VideoListItem
+            video = {video}
+            setVideo = {setVideo}
+            />
+          })}
+        </ul>
+    </aside>
     </>
   );
 };
--- src/components/VideoListItem/VideoListItem.jsx
+++ src/components/VideoListItem/VideoListItem.jsx
@@ -1,9 +1,15 @@
 import './VideoListItem.scss'
 
-const VideoListItem = () => {
+const VideoListItem = ({video, setVideo}) => {
 
   return (
-    <li>List item</li>
+    <li className='list-item'>
+      <img className='list-item__img' src={video.image} alt="next video thumbnail image" />
+      <article className='list-item__info'>
+        <p className='list-item__title'>{video.title}</p>
+        <p className='list-item__channel'>{video.channel}</p>
+      </article>
+    </li>
   )
 }
 export default VideoListItem;
