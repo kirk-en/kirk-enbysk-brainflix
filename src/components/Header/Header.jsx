57d3f3f5483b101b0db923d749a1945450925a52
Kirk Enbysk <kirkenbysk@gmail.com>
2024-06-01 16:22:27 -0400

    style header elements, add icon to search field, apply fixes to header flex

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,29 @@
-import logo from '../../assets/logo/BrainFlix-logo.svg'
-import profileImage from '../../assets/images/Mohan-muruge.jpg'
-import './Header.scss'
+import logo from "../../assets/logo/BrainFlix-logo.svg";
+import profileImage from "../../assets/images/Mohan-muruge.jpg";
+import icon from "../../assets/icons/upload.svg";
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <header className='header'>
-      <img className='header__logo' src={logo} alt="brainflix logo" />
+    <header className="header">
+      <figure className="header__logo-container">
+        <img className="header__logo" src={logo} alt="brainflix logo" />
+      </figure>
       <section className="header__right-controls">
-        <input className='header__input'></input>
-        <button>Upload</button>
-        <img className='header__profile-image' src={profileImage} alt="user profile image" />
+        <input className="header__input" placeholder="Search"></input>
+
+        <img
+          className="header__profile-image"
+          src={profileImage}
+          alt="user profile image"
+        />
+        <button className="header__btn">
+          <img className="header__icon" src={icon} alt="search bar icon" />
+          Upload
+        </button>
       </section>
     </header>
-  )
+  );
 };
 
 export default Header;
