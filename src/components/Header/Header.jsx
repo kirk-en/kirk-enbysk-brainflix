028c3edcbbccf58d977efd86ad31002f2a5c981a
Kirk Enbysk <kirkenbysk@gmail.com>
2024-06-03 16:26:58 -0400

    add routing to header items

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,13 @@ import logo from "../../assets/logo/BrainFlix-logo.svg";
 import profileImage from "../../assets/images/Mohan-muruge.jpg";
 import icon from "../../assets/icons/upload.svg";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <figure className="header__logo-container">
-        <img className="header__logo" src={logo} alt="brainflix logo" />
+        <Link to='/'><img className="header__logo" src={logo} alt="brainflix logo" /></Link>
       </figure>
       <section className="header__right-controls">
         <input className="header__input" placeholder="Search"></input>
@@ -17,10 +18,10 @@ const Header = () => {
           src={profileImage}
           alt="user profile image"
         />
-        <button className="header__btn">
-          <img className="header__icon" src={icon} alt="search bar icon" />
-          Upload
-        </button>
+        <Link to="/upload" className="header__btn">
+            <img className="header__icon" src={icon} alt="search bar icon" />
+            Upload
+        </Link>
       </section>
     </header>
   );
