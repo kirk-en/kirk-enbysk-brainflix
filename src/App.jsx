2947d6f77f710d71f579cb1163a3ced67225fa8d
Kirk Enbysk <kirkenbysk@gmail.com>
2024-06-03 15:46:03 -0400

    move app code to VideoPage file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import videosData from "./data/video-details.json";
 
 import Header from "./components/Header/Header";
-import Video from "./components/Video/Video";
-import VideoInfo from "./components/VideoInfo/VideoInfo";
-import VideoList from "./components/VideoList/VideoList";
-import CommentsContainer from "./components/CommentsContainer/CommentsContainer";
 
 console.log(videosData);
 
@@ -14,23 +11,9 @@ function App() {
   const [video, setVideo] = useState(videosData[0]);
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Video video={video} />
-      <div className="desktop-flex">
-        <div className="desktop-flex__left">
-          <VideoInfo video={video} />
-          <CommentsContainer video={video} />
-        </div>
-        <div className="desktop-flex__right">
-          <VideoList
-            videosData={videosData}
-            setVideo={setVideo}
-            currentVideoId={video.id}
-          />
-        </div>
-      </div>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/VideoPage/VideoPage.jsx
+++ src/Pages/VideoPage/VideoPage.jsx
@@ -1,13 +1,32 @@
-import './NEW.scss'
+import "./VideoPage.scss";
+import { useState } from "react";
+import videosData from "./data/video-details.json";
 
-const NEW = () => {
+import Video from "./components/Video/Video";
+import VideoInfo from "./components/VideoInfo/VideoInfo";
+import VideoList from "./components/VideoList/VideoList";
+import CommentsContainer from "./components/CommentsContainer/CommentsContainer";
 
+
+const VideoPage = () => {
   return (
-<>
+    <>
+      <Video video={video} />
+      <div className="desktop-flex">
+        <div className="desktop-flex__left">
+          <VideoInfo video={video} />
+          <CommentsContainer video={video} />
+        </div>
+        <div className="desktop-flex__right">
+          <VideoList
+            videosData={videosData}
+            setVideo={setVideo}
+            currentVideoId={video.id}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-</>
-
-  )
-}
-
-export default NEW;
+export default VideoPage;
