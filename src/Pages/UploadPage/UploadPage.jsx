205da17c5e90227e3b945c786d8a5e060f77c9fd
Kirk Enbysk <kirkenbysk@gmail.com>
2024-06-08 15:56:44 -0400

    refactor upload page component hierarchy

--- src/Pages/UploadPage/UploadPage.jsx
+++ src/Pages/UploadPage/UploadPage.jsx
@@ -1,12 +1,65 @@
+import thumbnail from "../../assets/images/Upload-video-preview.jpg";
+import icon from "../../assets/icons/publish.svg";
 import "./UploadPage.scss";
-import UploadForm from "../../components/UploadForm/UploadForm";
 import { Link } from "react-router-dom";
 
 const UploadPage = () => {
   return (
     <>
       <div className="divide-line"></div>
-      <UploadForm />
+      <section className="upload">
+        <h1 className="main-title upload__title">Upload Video</h1>
+        <div className="divide-line divide-line--upload-page"></div>
+        <form className="upload__form">
+          <div className="upload__thumbnail-container">
+            <p className="upload__subtitle upload__subtitle--top-gap">
+              video thumbnail
+            </p>
+            <img
+              src={thumbnail}
+              alt="Uploaded video thumbnail"
+              className="upload__image"
+            />
+          </div>
+          <div className="upload__text-areas-container">
+            <label htmlFor="title">
+              <p className="upload__subtitle">title your video</p>
+            </label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Add a title to your video"
+              className="text-input-field text-input-field--fixed-small"
+              required
+            />
+            <label htmlFor="description">
+              <p className="upload__subtitle">add a video description</p>
+            </label>
+            <textarea
+              type="text"
+              name="description"
+              placeholder="Add a description to your video"
+              className="text-input-field text-input-field--multi-line"
+              required
+            />
+          </div>
+          <div className="divide-line divide-line--upload-page"></div>
+          <div className="upload__buttons-container">
+            <button type="submit" className="upload__btn">
+              <img
+                className="upload__icon"
+                src={icon}
+                alt="publish video icon"
+              />
+              <span>publish</span>
+            </button>
+            <Link to={"/"} className="upload__btn upload__btn--cancel">
+              <span>cancel</span>
+            </Link>
+          </div>
+        </form>
+      </section>
+      );
     </>
   );
 };
